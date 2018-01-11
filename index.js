337b1b5fb036965a35503df2aa7dfc850aebb1b5
walkingalchemy <walkingalchemy@gmail.com>
2018-01-11 10:32:18 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
+class Driver {
+  constructor(name, startDate){
+    this.name = name
+    this.startDate = new Date(startDate)
+  }
 
+  yearsExperienceFromBeginningOf(year){
+    return Math.abs(this.startDate.getFullYear() - year)
+  }
+}
+
+
+class Route {
+  constructor(beginningLocation,endingLocation){
+    this.beginningLocation = beginningLocation
+    this.endingLocation = endingLocation
+  }
+
+  horizontalBlocksTravelled(){
+    let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+    return Math.abs(eastWest.indexOf(this.beginningLocation['horizontal']) - eastWest.indexOf(this.endingLocation['horizontal']))
+  }
+  verticalBlocksTravelled(){
+    return Math.abs(this.beginningLocation['vertical'] - this.endingLocation['vertical'])
+  }
+  blocksTravelled(){
+    return this.verticalBlocksTravelled() + this.horizontalBlocksTravelled()
+  }
+
+  estimatedTime(peakHours = false){
+    return peakHours ? this.blocksTravelled() / 2 : this.blocksTravelled() / 3
+  }
+}
